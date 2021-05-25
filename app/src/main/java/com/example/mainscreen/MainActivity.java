9c22c3034b2afa7e47d9fa8a6123144306abe456
fhsiam <80001466+fhsiam@users.noreply.github.com>
2021-05-26 04:04:10 +0600

    Main_screen

--- app/src/main/java/com/example/mainscreen/MainActivity.java
+++ app/src/main/java/com/example/mainscreen/MainActivity.java
@@ -40,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+
+
                     return true;
                 }
 
